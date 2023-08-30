39262e2babb8b54b1433d1fd8ba1c34faa48bb0a
Aldwin <aldwin828@gmail.com>
2023-08-30 13:47:11 -0400

    In progress: getting data from api

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+const { api_key } = require("./config");
+
 // API url
 const count = 10;
-const apiKey = '';
+const apiKey = api_key;
 const apiUrl =`https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// get photos from api
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        // Catch error
+    }
+}
+
+// On load
+getPhotos();
